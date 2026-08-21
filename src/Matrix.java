d2bbe5a27022170109db678277aac81a96c1c8d6
MSong27 <1234hhhfsf@gmail.com>
2026-08-20 22:04:29 -0700

    Gaussian Elimination and Determinant started

--- src/Matrix.java
+++ src/Matrix.java
@@ -7,7 +7,7 @@ public class Matrix {
     private int row;
     private int column;
     private double[][] matrix;
-    public static final double tolerance = 10e-9;
+    public static final double epsilon = 10e-9;
     public Matrix(int row, int column) {
         this.row = row;
         this.column = column;
@@ -31,6 +31,9 @@ public class Matrix {
             throw new IllegalArgumentException("Entry is not valid for current matrix");
         }
         else
+            if (entry == 0) {
+                entry = 0;
+            }
             matrix[row - 1][column - 1] = entry;
     }
 
@@ -40,6 +43,18 @@ public class Matrix {
         }
         else
             matrix = entry;
+    }
+
+    public void setEntries(Matrix entry) {
+        if (entry.row != this.row || column != entry.column){
+            throw new IllegalArgumentException("Entry is not valid for current matrix");
+        }
+        else
+            for (int i = 1 ; i <= this.row ; i++) {
+                for (int j = 1 ; j <= this.column ; j++) {
+                    setEntries(i, j, entry.getEntry(i, j));
+                }
+            }
     }
 
     public Matrix add(Matrix n) {
@@ -162,4 +177,105 @@ public class Matrix {
          }
          return this;
      }
+
+     public Matrix augment(Matrix matrixB) {
+         if (this.row != matrixB.getRow()) {
+             throw new IllegalArgumentException("Rows must be the same between the two matrices");
+         }
+         Matrix augmentedMatrix = new Matrix(this.row, matrixB.getColumn() + this.column);
+         for (int i = 1; i <= this.row; i++) {
+             for (int j = 1; j <= this.column; j++) {
+                 augmentedMatrix.setEntries(i, j, this.getEntry(i, j));
+             }
+         }
+         for (int i = 1; i <= matrixB.getRow(); i++) {
+             for (int j = 1; j <= matrixB.getColumn(); j++) {
+                 augmentedMatrix.setEntries(i, j + this.column, matrixB.getEntry(i, j));
+             }
+         }
+         return augmentedMatrix;
+     }
+
+     //Gaussian Elimination Algorithms
+
+    public boolean columnIsZero(int column) {
+        boolean isZero = true;
+        for (int i = 1 ; i <= this.row ; i++) {
+            if (this.getEntry(i, column) != 0) {
+                isZero = false;
+            }
+        }
+        return isZero;
+    }
+
+    //Errors IF there is a zero row
+
+    public Matrix gaussianElimination() {
+        int l = 1;
+        while (l <= this.row) {
+            int i = l;
+            while (i <= this.column) {
+                if (columnIsZero(i)) {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+
+            for (int j = l; j <= this.row; j++) {
+                if (Math.abs(this.getEntry(j, i)) > epsilon) {
+                    swap(l, j);
+                    rowScale(l, 1 / getEntry(l, i));
+                    break;
+                }
+            }
+
+            for (int k = l + 1; k <= this.row; k++) {
+                rowReplace(k, l, -1 * getEntry(k, i));
+            }
+            l++;
+        }
+        return this;
+    }
+
+    public double calculateDeterminant() {
+        if (this.row != this.column) {
+            throw new IllegalArgumentException("Matrix is not square");
+        }
+        Matrix triangularMatrix = new Matrix(this.row, this.column);
+        triangularMatrix.setEntries(this.matrix);
+        int l = 1;
+        int swapCounter = 0;
+        double scaleFactor = 1;
+        while (l <= triangularMatrix.getRow()) {
+            int i = l;
+            while (i <= triangularMatrix.getColumn()) {
+                if (columnIsZero(i)) {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+
+            for (int j = l; j <= triangularMatrix.getRow(); j++) {
+                if (Math.abs(triangularMatrix.getEntry(j, i)) > epsilon) {
+                    if (l != j) {
+                        swap(l, j);
+                        swapCounter++;
+                    }
+                    scaleFactor *= triangularMatrix.getEntry(l, i);
+                    rowScale(l, 1 / triangularMatrix.getEntry(l, i));
+                    break;
+                }
+            }
+
+            for (int k = l + 1; k <= this.row; k++) {
+                rowReplace(k, l, -1 * triangularMatrix.getEntry(k, i));
+            }
+            l++;
+        }
+        double determinant = 1;
+        return determinant * scaleFactor * Math.pow(-1, swapCounter);
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,20 @@
 public class Test {
     public static void main(String[] args) {
-        Matrix m = new Matrix(3, 1);
+        Matrix m = new Matrix(3, 3);
         Matrix n = new Matrix(1, 3);
         double[][] entryOne = {
-                {1},
-                {3},
-                {1}
+                {-3, 5, 2},
+                {2, -4,-1},
+                {-3, 0, 6}
+
         };
         double[][] entryTwo = {
                 {-1, 2, 5},
         };
         m.setEntries(entryOne);
         n.setEntries(entryTwo);
-        System.out.println(m.swap(1, 2));
+        System.out.println(m.calculateDeterminant());
+        System.out.println(m.gaussianElimination());
         System.out.print(Matrix.identity(5));
     }
 }
